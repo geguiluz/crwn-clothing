ed3e4174f60a82aaaa3127990853ba7fc6a0442b
geguiluz <memo.eguiluz@gmail.com>
2019-10-15 08:49:02 -0500

    Updated menu-items to navigate withRouter HOC

--- src/components/menu-item/menu.item.component.jsx
+++ src/components/menu-item/menu.item.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+import { withRouter } from 'react-router-dom';
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className='background-image'
         style={{
@@ -18,4 +23,8 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+// A higher order component is a function that takes a component, transforms it
+// and returns it with "super powers", i.e. access history, match and so on.
+// This way, we don't need to drill down properties through components that
+// don't need them
+export default withRouter(MenuItem);
